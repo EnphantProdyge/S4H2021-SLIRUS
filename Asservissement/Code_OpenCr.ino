d1c818fdd8f9d5452864109b6952cace93c95997
isaac-racine <67609784+isaac-racine@users.noreply.github.com>
2021-03-17 15:39:03 -0400

    com

--- Asservissement/Code_OpenCr.ino
+++ Asservissement/Code_OpenCr.ino
@@ -3,16 +3,32 @@
 
 void setup() {
   Serial.begin(57600);
-  COM_Arduino 
 }
-
+  COM_Arduino ROB;
+  String msg = "";
 void loop() {
-  
-  if(Serial.available()>0)
+
+
+  if (ROB.Get_state() == 1)// waiting for input
   {
-    msg = 
-    Serial.println(msg);
+    if(Serial.available()>0)// waiting for the serial port to receive an input 
+    {
+      msg = ROB.ReadMessage();//Reading the string sent
+      ROB.Get_status();// Sending the status
+      ROB.SendMessage(msg);//Sending the message received
+    }
   }
-    
+  
+  if (ROB.Get_state() == 2)
+  {
+    ROB.Set_state(3);
+    delay(10000);
+  }
+
+  if (ROB.Get_state() == 3)
+  {
+    ROB.SendMessage("IM DONE");
+  }
+  
   
 }
