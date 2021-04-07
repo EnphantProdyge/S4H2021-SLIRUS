170e899b87c4a9694c0a0f8994d42c0811fbb1cd
isaac-racine <67609784+isaac-racine@users.noreply.github.com>
2021-04-07 10:35:42 -0400

    Update Communication.h

--- Asservissement/Code_OpenCr/Communication.h
+++ Asservissement/Code_OpenCr/Communication.h
@@ -7,21 +7,24 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-
+//Class to initiate Serial communication with the Raspberry Pi 
 class COM_Arduino
 {
 public:
+  
+//Constructor
   COM_Arduino()
   {
-    //Constructor
    state = state1;// initial state 
   };
-
+  
+//Methode to send information(a String) to the Raspberry Pi
   void SendMessage(String str)
   {
     Serial.println(str);
   }
 
+//Methode to read information form the Raspberry Pi
   String ReadMessage()
   {
     String msg = Serial.readStringUntil('\n');
@@ -34,11 +37,13 @@ public:
     return msg;
   };
 
+//Methode to set a new state to the object
   void Set_state(int new_state)
   {
     state = new_state;
   };
 
+//Methode to get the state of the object
   int Get_state()
   {
     return state; 
@@ -58,8 +63,11 @@ public:
       Serial.println("Robot is doing a sign");
     } 
   };  
+  
 private:
+//Variable for the state of the object
   int state;
+//type of state possible
   int state1 = 1; // Waiting for input
   int state2 = 2; // Message received
   int state3 = 3; // robot is doing a sign
