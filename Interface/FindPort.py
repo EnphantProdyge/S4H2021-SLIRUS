40ea313a50c692bacc50d6e617d87c9ff269ddb5
isaac-racine <67609784+isaac-racine@users.noreply.github.com>
2021-04-07 10:56:29 -0400

    Update FindPort.py

--- Interface/FindPort.py
+++ Interface/FindPort.py
@@ -1,3 +1,4 @@
+#This code finds what Rapsberry Pi the serial port is used for the communication with the arduino and sends information by serial port.
 import sys
 import glob
 import serial
@@ -5,49 +6,51 @@ import serial.tools.list_ports
 import json
 import time
 
+#Function that finds the serial port used by the arduino
 def serial_ports():
-
+    #if on windows:
+    #Finds all ports available
     if sys.platform.startswith('win'):
         ports = list(serial.tools.list_ports.comports())
 
-
+    #if on linux:
+    #Finds all ports available
     elif sys.platform.startswith('linux'):
         ports = list(serial.tools.list_ports.comports())
+    #Send an error if the platform is unsupported
     else:
         raise EnvironmentError('Unsupported platform')
 
     result = []
     for port in ports:
-        j = 0
-        list2 = []
-        for i in port:
-            list2.insert(j,i)
-
-            j+=1
+        #if on windows:
         if sys.platform.startswith('win'):
             if "Arduino" in port[1] or "USB" in port[1]:
+                #Finds the port connected to the arduino
                 result.append(port[0])
+        #if on linux
         elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            #print(port[1])
             if "ACM0" in port[1] or "AMA0" in port[1]:
+                #Finds the port connected to the arduino
                 result.append(port[0])
+        #Send an error if the platform is unsupported
         else:
             raise EnvironmentError('Unsupported platform')
     return result
 
 
 def SendStrToOpenCR(port,msg):
-    
-    ser = serial.Serial("/dev/ttyACM0", 57600, timeout=1)
-    #ser = serial.Serial(port, 57600, timeout=1)
+    #Telling the code where to send the information(string)
+    ser = serial.Serial("/dev/ttyACM0", 57600, timeout=1) #try this line if the you dont receive anything form the arduino or if the arduino doesn't receive what you are sending
+    #ser = serial.Serial(port, 57600, timeout=1)          #else use this line
     ser.flush()
     line = ""
-
+    
+    #Sends the string to the arduino
     ser.write(msg.encode('utf-8'))
     while line != "IM DONE" :
-        #print(ser)
+        #Verifies if the arduino is sending information back
         line = ser.readline().decode('utf-8').rstrip()
-        #print("patate")
         print(line)
         time.sleep(1)
 
@@ -56,8 +59,5 @@ def SendStrToOpenCR(port,msg):
 if __name__ == '__main__':
     list = serial_ports()
     Str_port = list[0]
-    #print(Str_port)
     string = input("Quel est le string?:\t")
-    
-
     SendStrToOpenCR(Str_port,string)
