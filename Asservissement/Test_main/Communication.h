a18464a5c98cd01e9ecebe6787f08940843f0fed
beaum <beaf3009@usherbrookedev.ca>
2021-04-14 09:50:02 -0400

    commentaire dans le code d'asservissement

--- Asservissement/Test_main/Communication.h
+++ Asservissement/Test_main/Communication.h
@@ -1,27 +1,37 @@
-// Communication.h
-//
+/** @file Communication.h
+ * The file Communication.h contains the class COM_Arduino.
+ */
+
+ /** @class COM_Arduino
+  * @brief Class containing functions related to the communication.
+  *
+  * This class contains the state, the message of the communication between OpenCr and the code.
+  * @author RACINE, ISAAC
+  * @date 13/04/2021
+  */
 
 #ifndef _COMMUNICATION_H
 #define _COMMUNICATION_H
 
 #include <Arduino.h>
-//#include <ArduinoJson.h>
 
-
+///< Class to initiate serial communication with the raspberryPi
 class COM_Arduino
 {
 public:
+  ///< Constructor of class COM_Arduino
   COM_Arduino()
   {
-    //Constructor
    state = state1;// initial state 
   };
 
+  ///< Methode to send information (a string) to the raspberryPi
   void SendMessage(String str)
   {
     Serial.println(str);
   }
 
+  ///< Methode to read information from the raspberryPi
   String ReadMessage()
   {
     String msg = Serial.readStringUntil('\n');
@@ -34,15 +44,19 @@ public:
     return msg;
   };
 
+  //< Methode to set a new state to the object
   void Set_state(int new_state)
   {
     state = new_state;
   };
 
+  //< Methode to get the state of the object
   int Get_state()
   {
     return state; 
   };
+
+  //< Methode to get the status of the object
   void Get_status()
   {
       if (state == state1)
@@ -59,7 +73,9 @@ public:
     } 
   };  
 private:
+  //< Viariables for the state of the object
   int state;
+  // Types of state possible
   int state1 = 1; // Waiting for input
   int state2 = 2; // Message received
   int state3 = 3; // robot is doing a sign
--- Asservissement/Test_main/Test_main.ino
+++ Asservissement/Test_main/Test_main.ino
@@ -1,3 +1,10 @@
+/** @file Test_main.ino
+ * @brief This file is the main program of the motors control.
+ * 
+ * This file contains the main loop of the project.
+ * @author BEAUMIER, FANNY
+ * @date 13/04/2021
+ */
 #include <Wire.h>
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
@@ -5,30 +12,45 @@
 #include <DynamixelWorkbench.h>
 #include <string>
 #include "Communication.h"
+/**
+ * @headerfile Wire.h "Wire.h"
+ * @headerfile Arduino.h "Arduino.h"
+ * @headerfile Adafruit_PWMServoDriver.h "Adafruit_PWMServoDriver.h"
+ * @headerfile controlServo.h "controlServo.h"
+ * @headerfile DynamixelWorkbench.h "DynamixelWorkbench.h"
+ * @headerfile string.h "string.h"
+ * @headerfile Communication.h "Communication.h"
+ */
 
-#define FREQUENCY   50
-#define NB_FINGERS  5
-#define BAUDRATE  57600
+/** Communication variables:
+ */
+#define FREQUENCY   50                      ///< Frequency for the servo
+#define BAUDRATE  57600                     ///< Baud rate for Arduino mega 2560
+#define NB_FINGERS  5                       ///< Number of finger on the hand
 #if defined(__OPENCM904__)
-#define DEVICE_NAME "3" //Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
+#define DEVICE_NAME "3"                     ///< Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
 #elif defined(__OPENCR__)
 #define DEVICE_NAME ""
-#endif  
+#endif 
+COM_Arduino ROB; 
 
+/** command variables
+ */
 String msg = "";
 uint16_t model_number = 0;
 uint16_t model_number2 = 0;
 char result, result2;
 int32_t position = 0;
 
-COM_Arduino ROB;
+/** Initiating the Servo class object.
+ */
 Servo servo;
 
 void setup() {
   Serial.begin(BAUDRATE); 
-  //while (!Serial); // Wait for Opening Serial Monitor
 
-  //Initialisation Dinamixel
+  /** Initializing Dynamixel
+   */
   dxl_wb.init(DEVICE_NAME, BAUDRATE); //Initialisation du systeme qui gere les moteurs
   result = dxl_wb.ping(11, &model_number);
   result2 = dxl_wb.ping(12, &model_number2);
@@ -41,56 +63,60 @@ void setup() {
   dxl_wb.torqueOn(11);
   dxl_wb.torqueOn(12);
 
-  //Initialisation Servo
+  /** Initializing Servo
+   */
   pwm.begin();
   pwm.setPWMFreq(FREQUENCY);
   
-  //Initialisation base
+  /** Initializing base
+   */
   dxl_wb.itemRead(12,"Present_Position",&position);
-  servo.setupBase(position);
+  servo.setupBase(position);                ///< Set the base in the initial position (face front)
 
-  //Initialisation fingers and wrist
+  /** Initializing fingers and wrist
+   */
   for(int increment = 0; increment<NB_FINGERS; increment++){
-      servo.servoOut('5',increment);
+      servo.servoOut('5',increment);        ///< Set the hand in the initial position
   }
 }
 
 void loop() {
-  // string to individual caracteres
-  if (ROB.Get_state() == 1){// waiting for input
-    if(Serial.available()>0){// waiting for the serial port to receive an input 
-        msg = ROB.ReadMessage();//Reading the string sent
-        ROB.Get_status();// Sending the status
-        ROB.SendMessage(msg);//Sending the message received
+  /** Change string to individual caracteres
+   */
+  if (ROB.Get_state() == 1){                ///< Waiting for input
+    if(Serial.available()>0){               ///< Waiting for the serial port to receive an input 
+        msg = ROB.ReadMessage();            ///< Reading the string sent
+        ROB.Get_status();                   ///< Sending the status
+        ROB.SendMessage(msg);               ///< Sending the message received
     }
   }
-  if (ROB.Get_state() == 2){
+  if (ROB.Get_state() == 2){                ///< The hand is executing a message
     delay(500);
-    char caracteres[sizeof(msg)+1];
-    strcpy(caracteres, msg.c_str());
+    char caracteres[sizeof(msg)+1];         ///< Creation of a char array that will containt all the letters of the message
+    strcpy(caracteres, msg.c_str());        ///< Transfering the message to a string then in different letter in the char array
     for (int i = 0; i < (msg.length()); i++){
-      delay(2000);
-      Serial.print("Interface:start:"); //Message to interface : start letter
+      delay(2000);                          ///< Delay between each letter, in initial position
+      Serial.print("Interface:start:");     ///< Message to interface : start letter
       Serial.println(caracteres[i]);
       for(int increment = 0; increment<NB_FINGERS; increment++){
-        servo.servoOut(caracteres[i],increment);
+        servo.servoOut(caracteres[i],increment); ///< Set the hand in the letter position
       }
-      delay(2000);
+      delay(2000);                          ///< Time of the letter in position
         for(int decrement = NB_FINGERS-1; decrement >= 0; decrement--){
-          servo.reverseMove(caracteres[i],decrement);
+          servo.reverseMove(caracteres[i],decrement); ///< Replace the hand in the initial position
         }
-      Serial.print("Interface:done:");//Message to interface : finish letter
+      Serial.print("Interface:done:");      ///< Message to interface : finish letter
       Serial.println(caracteres[i]);
     }
-   ROB.Set_state(3);
+   ROB.Set_state(3);                        ///< All the letter of the string/message are done
     
   }
-  if (ROB.Get_state() == 3){  
+  if (ROB.Get_state() == 3){                ///< All the letter of the string/message are done
     delay(500);
-    Serial.println("Interface:exit");
+    Serial.println("Interface:exit");       ///< Message to interface : exit demonstration
     ROB.SendMessage("IM DONE");
-    msg = "";
-    ROB.Set_state(1);
+    msg = "";                               ///< Setting the message to empty
+    ROB.Set_state(1);                       ///< The hand is ready to receive another message
     
   }    
   
